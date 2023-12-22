5623326e72098cbbca09109e5f5dc5c8b1e31fb8
SirGram <119138378+SirGram@users.noreply.github.com>
2023-12-22 03:58:23 +0100

    add function, some css, visibility, deletion

--- javascript/library/script.js
+++ javascript/library/script.js
@@ -1,54 +1,159 @@
-const books = [];
+let books = [
+  {
+    image:
+      "https://m.media-amazon.com/images/I/614bH2xxR9L._AC_UF1000,1000_QL80_.jpg",
+    title: "The Republic of Thieves",
+    author: "Scott Lynch",
+    numPages: 600,
+    numPagesRead: 500,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/41WT7xwJJBS.jpg",
+    title: "Dark Imperium",
+    author: "Guy Haley",
+    numPages: 400,
+    numPagesRead: 400,
+  },
+  {
+    image:
+      "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1663778488-51vYL46dfL._SL500_.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    title: "The Eye of the World",
+    author: "Robert Jordan",
+    numPages: 680,
+    numPagesRead: 200,
+  },
+  {
+    image:
+      "https://i1.wp.com/www.getepic.com/learn/wp-content/uploads/2021/04/The-Girl-Who-Drank-the-Moon.jpeg?resize=584%2C886&ssl=1",
+    title: "The Girl who Drank the Moon",
+    author: "Kelly Barnhill",
+    numPages: 200,
+    numPagesRead: 197,
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1678895127i/123163147.jpg",
+    title: "The Future",
+    author: "Naomi Alderman",
+    numPages: 332,
+    numPagesRead: 19,
+  },
+];
+const unavailableCover =
+  "https://blog.springshare.com/wp-content/uploads/2010/02/nc-md.gif";
 
-function Book(title, author, numPages, numPagesRead) {
+function Book(image, title, author, numPages, numPagesRead) {
+  this.image = image;
   this.title = title;
   this.author = author;
   this.numPages = numPages;
   this.numPagesRead = numPagesRead;
 }
 function addBookToLibrary() {
+  image = $image.value;
   title = $title.value;
   author = $author.value;
   numPages = $numPages.value;
   numPagesRead = $numPagesRead.value;
-  newBook = new Book(title, author, numPages, numPagesRead);
-  console.log(newBook)
+  newBook = new Book(image, title, author, numPages, numPagesRead);
+
   books.push(newBook);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   //Selectors
   $form = document.querySelector("#form");
+  $image = $form.querySelector("#image");
   $title = $form.querySelector("#title");
   $author = $form.querySelector("#author");
-  $numPages = $form.querySelector("#total-pages");
-  $numPagesRead = $form.querySelector("#pages-read");
+  $numPages = $form.querySelector("#numPages");
+  $numPagesRead = $form.querySelector("#numPagesRead");
 
-  const bookContainer = document.getElementById("book-container");
-  console.log(bookContainer);
-
-  const submission = document.getElementById("submit-button");
-  submission.addEventListener("click", function () {
+  //Add new books
+  const addBookButton = document.getElementById("submit-button");
+  addBookButton.addEventListener("click", function () {
     console.log("click");
     addBookToLibrary();
-    console.log(books)
+    console.log(books);
+    displayBooks(books);
   });
 
-  for (book of books) {
-    let bookData = document.createElement("div");
-    bookData.className = "book-data";
+  displayBooks(books);
 
-    let bookTitle = document.createElement("div");
-    bookTitle.className = "book-title";
-    bookTitle.innerHTML = book.title;
+  //Toggle Hidden
+  $toggleHidden = document.querySelector("#toggle-hidden");
 
-    bookData.appendChild(bookTitle);
+  $bookContainer = document.querySelector("#book-container");
+  $toggleHidden.addEventListener("click", function () {
+    $form.classList.toggle("hidden");
+    $bookContainer.style.display =
+      $bookContainer.style.display === "none" ? "grid" : "none";
+  });
 
-    let bookAuthor = document.createElement("div");
-    bookAuthor.className = "book-author";
-    bookAuthor.innerHTML = book.author;
-    bookData.appendChild(bookAuthor);
+  function displayBooks(books) {
+    const bookContainer = document.getElementById("book-container");
+    console.log("boooks", bookContainer);
+    bookContainer.innerHTML = "";
+    for (const [index, book] of books.entries()) {
+      let bookData = document.createElement("div");
+      bookData.className = "book-data";
+      bookData.id = `book-${index}`;
 
-    bookContainer.appendChild(bookData);
+      let bookImage = document.createElement("img");
+      bookImage.className = "book-image";
+      console.log(book.image);
+      if (book.image === "") {
+        bookImage.src = unavailableCover;
+      } else {
+        bookImage.src = book.image;
+      }
+      bookData.appendChild(bookImage);
+      let bookLabel = document.createElement("div");
+      let labelText;
+      if (book.numPagesRead - book.numPages === 0) {
+        bookImage.style.filter = "grayscale(100%)";
+        labelText = document.createTextNode("READ");
+        bookLabel.style.width="200px"
+        
+        bookLabel.style.padding="0.5rem 0 "
+      } else {
+        let percentage = Math.round((book.numPagesRead / book.numPages) * 100);
+        labelText = document.createTextNode(`${percentage} %`);
+      }
+      bookLabel.className = "read-label";
+      bookLabel.appendChild(labelText);
+
+      bookData.appendChild(bookLabel);
+
+      let bookRemove = document.createElement("button");
+      bookRemove.className = "book-remove";
+      
+      let bookRemoveIcon = document.createElement("img");
+      bookRemoveIcon.src = 'src/delete.png'
+      bookRemove.addEventListener("click", function () {
+        removeBook(index);
+      });
+      bookRemove.appendChild(bookRemoveIcon)
+      bookData.appendChild(bookRemove);
+
+      bookContainer.appendChild(bookData);
+    }
+  }
+
+  function removeBook(index) {
+    books.splice(index, 1);
+    displayBooks(books);
   }
 });
+
+/*  
+  let bookTitle = document.createElement("div");
+      bookTitle.className = "book-title";
+      bookTitle.innerHTML = book.title;
+
+      bookData.appendChild(bookTitle);
+
+      let bookAuthor = document.createElement("div");
+      bookAuthor.className = "book-author";
+      bookAuthor.innerHTML = book.author;
+      bookData.appendChild(bookAuthor); */
