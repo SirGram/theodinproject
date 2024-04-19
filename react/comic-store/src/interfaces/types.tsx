513751ec01c94b4dba195dea1c8376b5954fdb41
sirgram <gabrieulogio@gmail.com>
2024-04-19 19:15:21 +0200

    edited fetchItems

---
+++ react/comic-store/src/interfaces/types.tsx
@@ -0,0 +1,11 @@
+export type ComicDate = "thisWeek" | "lastWeek" | "thisMonth" | "nextWeek";
+export type ComicLimit = 10 | 20 | 50 | 100;
+export type ComicFormat =
+  | "comic"
+  | "magazine"
+  | "trade paperback"
+  | "hardcover"
+  | "digest"
+  | "graphical novel"
+  | "digital comic"
+  | "infinite comic";
